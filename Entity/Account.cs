d6c0ee3f3a671f637ad0738b7714da3e8d7d085f
Ali Khadempour <ali.khadempour@outlook.com>
2023-09-09 23:55:03 +0330

    create account entity

---
+++ Entity/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Entity.Contracts;
+
+namespace Entity
+{
+    public class Account : IAccount
+    {
+        #region Fields
+        public int ID { get; set; }
+        public string AccountNumber { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public long Balance { get; set; }
+        public List<Customer> Owners { get; set; }
+        public int MinAmount { get; set; }
+        #endregion
+
+        #region Constructor
+        public Account(int id, string accountNumber, DateTime openingDate, List<Customer> owners, int minAmount)
+        {
+            ID = id;
+            AccountNumber = accountNumber;
+            OpeningDate = openingDate;
+            Balance = minAmount;
+            Owners = owners;
+            MinAmount = minAmount;
+        }
+        #endregion
+    }
+}
---
+++ Entity/Contracts/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity.Contracts
+{
+    public interface IAccount
+    {
+        #region Fields
+        int ID { get; set; }
+        string AccountNumber { get; set; }
+        DateTime OpeningDate { get; set; }
+        long Balance { get; set; }
+        List<Customer> Owners { get; set; }
+        int MinAmount { get; set; }
+        #endregion
+    }
+}
