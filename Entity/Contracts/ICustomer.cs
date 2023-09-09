c544ec6b824c7e061c6dbf70885356038aa1ed9e
Ali Khadempour <ali.khadempour@outlook.com>
2023-09-09 22:08:22 +0330

    create entity and customer class in entity

---
+++ Entity/Contracts/ICustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entity.Contracts
+{
+    public interface ICustomer
+    {
+        #region Fields
+        int ID { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime BirthDate { get; set; }
+        string NationalID { get; set; }
+        string MobileNumber { get; set; }
+        string Address { get; set; }
+        string EmailAddress { get; set; }
+        string Nationality { get; set; }
+        #endregion
+    }
+}
---
+++ Entity/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Entity.Contracts;
+
+namespace Entity
+{
+    public class Customer : ICustomer
+    {
+        #region
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string NationalID { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string EmailAddress { get; set; }
+        public string Nationality { get; set; }
+        #endregion
+
+        #region Constructor
+        public Customer(int id, string firstName, string lastName, DateTime birthDate, string nationalID,
+                        string mobileNumber, string address, string emailAddress, string nationality)
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            NationalID = nationalID;
+            MobileNumber = mobileNumber;
+            Address = address;
+            EmailAddress = emailAddress;
+            Nationality = nationality;
+        }
+        #endregion
+    }
+}
