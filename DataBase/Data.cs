8c759bed90476940c372d3a5c6d8d57d1db88b6c
Ali Khadempour <ali.khadempour@outlook.com>
2023-09-10 22:18:50 +0330

    create DataBase

---
+++ DataBase/Data.cs
@@ -0,0 +1,22 @@
+﻿
+using Entity;
+using System.Collections.Generic;
+
+namespace DataBase
+{
+    public class Data
+    {
+        #region Fields
+        public static List<Customer> customers;
+        #endregion
+
+
+        #region Constructor
+        static Data()
+        {
+            customers = new List<Customer>();
+        }
+        #endregion
+
+    }
+}
