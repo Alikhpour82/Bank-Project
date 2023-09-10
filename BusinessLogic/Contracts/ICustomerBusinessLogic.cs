039b5e2d17e7ffc4a788c3292e4ecf8a2cefd76f
Ali Khadempour <ali.khadempour@outlook.com>
2023-09-10 22:19:31 +0330

    create business logic layer and create customer business logic

---
+++ BusinessLogic/Contracts/ICustomerBusinessLogic.cs
@@ -0,0 +1,13 @@
+﻿using Entity;
+
+namespace BusinessLogic.Contracts
+{
+    public interface ICustomerBusinessLogic
+    {
+        #region Methods
+        void AddCustomer();
+        Customer FindByID(int id);
+        int GenerateCustomerID();
+        #endregion
+    }
+}
---
+++ BusinessLogic/CustomerBusinessLogic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Entity;
+using DataBase;
+using BusinessLogic.Contracts;
+
+namespace BusinessLogic
+{
+    public class CustomerBusinessLogic : ICustomerBusinessLogic
+    {
+        #region Fields
+        private static int _customerID;
+        #endregion
+
+        #region Methods
+        public void AddCustomer()
+        {
+            int customerID = GenerateCustomerID();
+            Console.WriteLine($"Customer ID: {customerID}");
+
+            Console.Write("Enter customer first name: ");
+            string customerFirstName = Console.ReadLine();
+
+            Console.Write("Enter customer last name: ");
+            string customerLastName = Console.ReadLine();
+
+            Console.Write("Enter customer birth date: ");
+            DateTime customerBirthDate = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("Enter customer national ID: ");
+            string customerNationalID = Console.ReadLine();
+
+            Console.Write("Enter customer mobile number: ");
+            string customerMobileNumber = Console.ReadLine();
+
+            Console.Write("Enter customer address: ");
+            string customerAddress = Console.ReadLine();
+
+            Console.Write("Enter customer emailAddress: ");
+            string customerEmailAddress = Console.ReadLine();
+
+            Console.Write("Enter customer nationality: ");
+            string customerNationality = Console.ReadLine();
+
+
+            Data.customers.Add(new Customer(
+                customerID,
+                customerFirstName,
+                customerLastName,
+                customerBirthDate,
+                customerNationalID,
+                customerMobileNumber,
+                customerAddress,
+                customerEmailAddress,
+                customerNationality));
+        }
+
+        public Customer FindByID(int id)
+        {
+            foreach (Customer customer in Data.customers)
+            {
+                if (customer.ID == id)
+                {
+                    return customer;
+                }
+            }
+            return null;
+        }
+
+        public int GenerateCustomerID()
+        {
+            _customerID++;
+            return _customerID;
+        }
+        #endregion
+
+        #region Constroctor
+        static CustomerBusinessLogic()
+        {
+            _customerID = 0;
+        }
+        #endregion
+    }
+}
